2fa1db3feac79560f3a5794e32eb30376478438b
Matthieu CROUZET <Matthieu CROUZET>
2016-10-19 16:24:44 -0400

    Person : getter and setter

--- Exercice1/Exercice1/Person.h
+++ Exercice1/Exercice1/Person.h
@@ -10,6 +10,31 @@ class Person
 public:
 	Person();
 	~Person();
+
+
+	//getter
+	std::string getFirstName()	{ return m_firstName;	};
+	std::string getLastName()	{ return m_lastName;	};
+	int			getBirthYear()	{ return m_birthYear;	};
+	Color		getEyesColor()	{ return m_eyesColor;	};
+	Person*		getFather()		{ return m_father;		};	
+	Person*		getMonther()	{ return m_mother;		};
+	Person*		getBrotherL()	{ return m_brotherL;	};
+	Person*		getBrotherR()	{ return m_brotherR;	};
+	Person*		getChild()		{ return m_child;		};
+
+	//setter
+	void setFirstName(std::string firstName)	{ m_firstName = firstName;	};
+	void setLastName(std::string lastName)		{ m_lastName = lastName;	};
+	void setBirthYear(int birthYear)			{ m_birthYear = birthYear;	};
+	void setEyesColor(Color eyesColor)			{ m_eyesColor = eyesColor;	};
+	void setFather(Person* father)				{ m_father = father;		};
+	void setMonther(Person* mother)				{ m_mother = mother;		};
+	void setBrotherL(Person* brotherL)			{ m_brotherL = brotherL;	};
+	void setBrotherR(Person* brotherR)			{ m_brotherR = brotherR;	};
+	void setChild(Person* child)				{ m_child = child;			};
+
+
 private:
 	std::string	m_firstName;
 	std::string m_lastName;
